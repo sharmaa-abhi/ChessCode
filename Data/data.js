ddf70d172f6e8c9e8f3461cbc55b795697214b16
sharmaa-abhi <sharmaa24434@gamil.com>
2026-03-26 17:13:04 +0530

    initial start with render

---
+++ Data/data.js
@@ -0,0 +1,12 @@
+function Greet() {
+  alert("Hello");
+}
+
+// for each square
+function Square(color, piece, id) {
+  return { color, piece, id };
+}
+
+function initBoard() {}
+
+export { initBoard };
---
+++ index.js
@@ -0,0 +1,8 @@
+/*import { Greet } from "./data/data.js";
+Greet();
+
+import { Square } from "./data/data.js";
+console.log(Square("white", "pawn", "a2"));
+
+*/
+// above code is for testing purposes, will be removed in the future.
