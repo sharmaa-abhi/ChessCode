df9422af4e01448ecdb218a4be195cb573be932d
sharmaa-abhi <sharmaa24434@gamil.com>
2026-03-31 21:51:41 +0530

    All pieces occur on chessBoard

--- Data/pieces.js
+++ Data/pieces.js
@@ -1,3 +1,4 @@
+// black pieces
 function blackPawn(current_Position) {
   return {
     current_Position,
@@ -5,6 +6,42 @@ function blackPawn(current_Position) {
   };
 }
 
+function blackRook(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/black/rook.png",
+  };
+}
+
+function blackBishop(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/black/bishop.png",
+  };
+}
+
+function blackQueen(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/black/queen.png",
+  };
+}
+
+function blackKing(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/black/king.png",
+  };
+}
+
+function blackKnight(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/black/knight.png",
+  };
+}
+
+// white pieces
 function whitePawn(current_Position) {
   return {
     current_Position,
@@ -12,4 +49,50 @@ function whitePawn(current_Position) {
   };
 }
 
-export { blackPawn, whitePawn };
+function whiteRook(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/white/rook.png",
+  };
+}
+
+function whiteKnight(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/white/knight.png",
+  };
+}
+
+function whiteBishop(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/white/bishop.png",
+  };
+}
+function whiteKing(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/white/king.png",
+  };
+}
+function whiteQueen(current_Position) {
+  return {
+    current_Position,
+    img: "./Assets/Pieces/white/queen.png",
+  };
+}
+
+export {
+  blackPawn,
+  whitePawn,
+  blackRook,
+  whiteRook,
+  blackKnight,
+  whiteKnight,
+  blackBishop,
+  whiteBishop,
+  blackKing,
+  whiteKing,
+  blackQueen,
+  whiteQueen,
+};
--- Render/main.js
+++ Render/main.js
@@ -1,5 +1,7 @@
-import { blackPawn } from "../Data/pieces.js";
-import { whitePawn } from "../Data/pieces.js";
+// import { blackPawn } from "../Data/pieces.js";
+// import { whitePawn } from "../Data/pieces.js";
+
+import * as pieces from "../Data/pieces.js";
 
 const ROOT_DIV = document.getElementById("root");
 // Use when you want to render pieces on board (after first time when you start game).
@@ -43,14 +45,61 @@ function initGameRender(data) {
 
       // Render blackPawn
       if (square.id[1] == 7) {
-        square.piece = blackPawn(square.id);
+        square.piece = pieces.blackPawn(square.id);
         // console.log("black pawn,", square.piece);
+      }
+
+      // Render blackKnight
+      if (square.id == "b8" || square.id == "g8") {
+        square.piece = pieces.blackKnight(square.id);
+      }
+      // Render blackRook
+      if (square.id == "h8" || square.id == "a8") {
+        square.piece = pieces.blackRook(square.id);
+      }
+
+      // Render blackBishop
+      if (square.id == "c8" || square.id == "f8") {
+        square.piece = pieces.blackBishop(square.id);
+      }
+      // Render blackQueen
+      if (square.id == "d8") {
+        square.piece = pieces.blackQueen(square.id);
+      }
+      // Render blackKing
+      if (square.id == "e8") {
+        square.piece = pieces.blackKing(square.id);
       }
 
       // Render whitePawn
       if (square.id[1] == 2) {
-        square.piece = whitePawn(square.id);
+        square.piece = pieces.whitePawn(square.id);
         // console.log("white pawn,", square.piece);
+      }
+
+      // Render whiteKnight
+      if (square.id == "b1" || square.id == "g1") {
+        square.piece = pieces.whiteKnight(square.id);
+      }
+
+      // Render whiteRook
+      if (square.id == "h1" || square.id == "a1") {
+        square.piece = pieces.whiteRook(square.id);
+      }
+
+      // Render whiteBishop
+      if (square.id == "c1" || square.id == "f1") {
+        square.piece = pieces.whiteBishop(square.id);
+      }
+
+      // Render whiteQueen
+      if (square.id == "d1") {
+        square.piece = pieces.whiteQueen(square.id);
+      }
+
+      // Render whiteKing
+      if (square.id == "e1") {
+        square.piece = pieces.whiteKing(square.id);
       }
 
       rowElement.appendChild(squareDiv);
